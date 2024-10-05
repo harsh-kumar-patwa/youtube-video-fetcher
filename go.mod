844955a18d16795682ba2ce2e4dfdb17622ba8a7
Harsh Kumar <harshkumar3446@gmail.com>
2024-10-05 11:15:15 +0530

    Initial project setup with basic structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harsh-kumar-patwa/youtube-video-fetcher
+
+go 1.23.2
---
+++ server/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Starting YouTube API project")
+	log.Println("Server initialized")
+}
